104efef04501fc6a4d4159e71b96154199a54b00
Juan Cruz <juanurquiza96@gmail.com>
2019-05-04 12:56:28 -0300

    se agrego mensaje de error de token

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -16,7 +16,9 @@ let verificaToken = (req, res, next) => {
         if (err) {
             return res.status(401).json({
                 ok: false,
-                err
+                err: {
+                    message: "token invalido"
+                }
             });
         }
 
